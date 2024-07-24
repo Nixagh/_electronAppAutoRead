60a1ef6d68bcb897dbb65ebb3d5064a16e1d133d
Nixagh <114222149+Nixagh@users.noreply.github.com>
2024-07-24 14:40:54 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,101 +1,114 @@
 const {
-	contextBridge,
-	ipcRenderer
+    contextBridge,
+    ipcRenderer
 } = require('electron');
 
 // set local storage
 localStorage.setItem('autoScroll', 'true');
+alert = () => {};
+
+// clear ajaxHelper
+const clearAjaxHelper = () => {
+    AjaxHelper.setLoadWaiting = () => {};
+    AjaxHelper.processAjaxError = (e) => {
+        setTimeout((function() {
+            location.reload()
+        }), 100)
+    }
+}
+
+clearAjaxHelper();
 
 document.addEventListener('DOMContentLoaded', () => {
-	try {
-		const images = document.querySelectorAll('img');
-		// delete all images
-		images.forEach((image) => image.remove());
+    try {
+        const images = document.querySelectorAll('img');
+        // delete all images
+        images.forEach((image) => image.remove());
 
-		alert = () => {}
+        alert = () => {}
 
-		let autoScroll = true;
+        let autoScroll = true;
 
-		const initComponent = () => {
-			const myComponent = document.createElement('div');
-			myComponent.id = 'my-component';
-			document.body.appendChild(myComponent);
+        const initComponent = () => {
+            const myComponent = document.createElement('div');
+            myComponent.id = 'my-component';
+            document.body.appendChild(myComponent);
 
-			return myComponent;
-		}
+            return myComponent;
+        }
 
-		const initButton = (myComponent) => {
-			// add auto scroll button
-			const autoScrollButton = document.createElement('button');
-			autoScrollButton.id = 'auto-scroll-button';
-			autoScrollButton.innerText = 'Auto scroll';
+        const initButton = (myComponent) => {
+            // add auto scroll button
+            const autoScrollButton = document.createElement('button');
+            autoScrollButton.id = 'auto-scroll-button';
+            autoScrollButton.innerText = 'Auto scroll';
 
-			// button always sticks to the bottom right corner
-			autoScrollButton.style.position = 'fixed';
-			autoScrollButton.style.bottom = '0';
-			autoScrollButton.style.right = '100px';
-			// add style
-			autoScrollButton.style.backgroundColor = localStorage.getItem('autoScroll') === 'true' ? 'green' : 'red';
-			autoScrollButton.style.color = 'white';
-			autoScrollButton.style.border = 'none';
-			autoScrollButton.style.padding = '2px';
-			autoScrollButton.style.borderRadius = '5px';
-			autoScrollButton.style.cursor = 'pointer';
-			// z-index
-			autoScrollButton.style.zIndex = '9999';
+            // button always sticks to the bottom right corner
+            autoScrollButton.style.position = 'fixed';
+            autoScrollButton.style.bottom = '0';
+            autoScrollButton.style.right = '100px';
+            // add style
+            autoScrollButton.style.backgroundColor = localStorage.getItem('autoScroll') === 'true' ? 'green' : 'red';
+            autoScrollButton.style.color = 'white';
+            autoScrollButton.style.border = 'none';
+            autoScrollButton.style.padding = '2px';
+            autoScrollButton.style.borderRadius = '5px';
+            autoScrollButton.style.cursor = 'pointer';
+            // z-index
+            autoScrollButton.style.zIndex = '9999';
 
-			// width and height scale with window size
-			autoScrollButton.style.width = window.innerWidth * 0.3 + 'px';
-			autoScrollButton.style.height = window.innerHeight * 0.05 + 'px';
+            // width and height scale with window size
+            autoScrollButton.style.width = window.innerWidth * 0.3 + 'px';
+            autoScrollButton.style.height = window.innerHeight * 0.05 + 'px';
 
-			myComponent.appendChild(autoScrollButton);
+            myComponent.appendChild(autoScrollButton);
 
-			return autoScrollButton;
-		}
+            return autoScrollButton;
+        }
 
-		const initButtonEvent = (autoScrollButton) => {
-			// add event listener
-			autoScrollButton.addEventListener('click', async () => {
-				autoScroll = !autoScroll;
-				if (autoScroll) {
-					autoScrollButton.style.backgroundColor = 'green';
-					// add local storage
-					localStorage.setItem('autoScroll', 'true');
+        const initButtonEvent = (autoScrollButton) => {
+            // add event listener
+            autoScrollButton.addEventListener('click', async () => {
+                autoScroll = !autoScroll;
+                if (autoScroll) {
+                    autoScrollButton.style.backgroundColor = 'green';
+                    // add local storage
+                    localStorage.setItem('autoScroll', 'true');
 
-					await clickNextButton(false);
-				} else {
-					autoScrollButton.style.backgroundColor = 'red';
-					localStorage.setItem('autoScroll', 'false');
-				}
-			});
-		}
+                    await clickNextButton(false);
+                } else {
+                    autoScrollButton.style.backgroundColor = 'red';
+                    localStorage.setItem('autoScroll', 'false');
+                }
+            });
+        }
 
-		const clickNextButton = async (wait = true) => {
-			const nextButton = document.querySelector('.next');
+        const clickNextButton = async (wait = true) => {
+            const nextButton = document.querySelector('.next');
 
-			if (nextButton.classList.contains('disabled')) {
-				ipcRenderer.send('reaad-done', 'read done');
-			}
+            if (nextButton.classList.contains('disabled')) {
+                ipcRenderer.send('reaad-done', 'read done');
+            }
 
-			if (nextButton) {
-				setTimeout(() => {
-					ipcRenderer.send('next', location.href);
-					nextButton.click();
-				}, 7.1 * 1000)
-			} else {
-				console.log('next button not found');
-				location.reload();
-			}
-		}
+            if (nextButton) {
+                setTimeout(() => {
+                    ipcRenderer.send('next', location.href);
+                    nextButton.click();
+                }, 7.1 * 1000)
+            } else {
+                console.log('next button not found');
+                location.reload();
+            }
+        }
 
-		const component = initComponent();
-		const button = initButton(component);
-		initButtonEvent(button);
+        const component = initComponent();
+        const button = initButton(component);
+        initButtonEvent(button);
 
-		if (localStorage.getItem('autoScroll') === 'true') {
-			clickNextButton().then();
-		}
-	} catch (e) {
-		location.reload();
-	}
+        if (localStorage.getItem('autoScroll') === 'true') {
+            clickNextButton().then();
+        }
+    } catch (e) {
+        location.reload();
+    }
 });
