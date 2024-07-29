98eb37f80dcbbd32b1f27976561c57c579b9d7d2
Nixagh <114222149+Nixagh@users.noreply.github.com>
2024-07-29 22:41:00 +0000

    change

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+docker run --privileged --shm-size 1g -d -p 2000:10000 -e VNC_PASSWD=password -e PORT=10000 -e AUDIO_PORT=1699 -e WEBSOCKIFY_PORT=6900 -e VNC_PORT=5900 -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 -e SCREEN_DEPTH=24 thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+docker run --privileged --shm-size 1g -d -p 2001:10000 -e VNC_PASSWD=password -e PORT=10000 -e AUDIO_PORT=1699 -e WEBSOCKIFY_PORT=6900 -e VNC_PORT=5900 -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 -e SCREEN_DEPTH=24 thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+docker run --privileged --shm-size 1g -d -p 2002:10000 -e VNC_PASSWD=password -e PORT=10000 -e AUDIO_PORT=1699 -e WEBSOCKIFY_PORT=6900 -e VNC_PORT=5900 -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 -e SCREEN_DEPTH=24 thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 nvm install node
+wget https://github.com/Nixagh/_electronAppAutoRead/archive/refs/heads/master.zip
+unzip master.zip
+rm master.zip
+cd _electronAppAutoRead-master
+npm install
+clear
+npm start
